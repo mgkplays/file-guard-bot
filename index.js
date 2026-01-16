2f2a64cc42a0da5d0dd68e4d485ba6f5e62e371c
mgkplays <mgkplayxz@gmail.com>
2026-01-16 20:35:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -56,3 +56,14 @@ ${fileNames}`
 });
 
 client.login(TOKEN);
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
